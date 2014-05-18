2a85763973460e3fac88b442ca12305fcc820a64
fucky0925 <gity0925@gmail.com>
2014-05-18 17:25:31 +0900

    Initial commit

---
+++ src/com/mycompany/myapp4/Info.java
@@ -0,0 +1,12 @@
+package com.mycompany.myapp4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Info extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.info);
+	}
+}
